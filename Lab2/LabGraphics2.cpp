5be50277cdba22919f37b4c30c35d5dde6a4de15
Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-03-09 16:44:29 +0200

    new

---
+++ Lab2/LabGraphics2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+HWND hwnd = GetConsoleWindow();
+HDC hdc = GetDC(hwnd);
+RECT window = {};
+const double PI = 3.1415926535897932384;
+
+//tregtr
+void drawPifagorsTree(int x, int y, int l, double radian)
+{
+    if (l <= 20)
+        return;
+
+    MoveToEx(hdc, x, y, NULL);
+    LineTo(hdc, x + l * cos(radian + PI / 6), y - l * sin(radian + PI / 6));
+
+    MoveToEx(hdc, x, y, NULL);
+    LineTo(hdc, x + l * cos(radian - PI / 6), y - l * sin(radian - PI / 6));
+
+    drawPifagorsTree(x + l * cos(radian + PI / 6), y - l * sin(radian + PI / 6), l / 1.3, radian + PI / 6);
+    drawPifagorsTree(x + l * cos(radian - PI / 6), y - l * sin(radian - PI / 6), l / 1.3, radian - PI / 6);
+}
+
+int main()
+{
+    SelectObject(hdc, GetStockObject(WHITE_PEN));
+    SelectObject(hdc, GetStockObject(WHITE_BRUSH));
+
+    int x = 1000, y = 1000, l = 150;
+    double radian = PI / 2;
+    std::getchar();
+
+    MoveToEx(hdc, x, y, NULL);
+    y -= l * sin(radian);
+    LineTo(hdc, x, y);
+
+    l /= 1.3;
+
+    drawPifagorsTree(x, y, l, radian);
+
+    ReleaseDC(hwnd, hdc);
+
+    std::cin >> x;
+    return 0;
+}
