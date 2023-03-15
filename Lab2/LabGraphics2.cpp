bbfa48534a34ba403bec2890be1ab6871d4eed25
Kiiashchenko_Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-03-15 10:09:06 +0200

    color

--- Lab2/LabGraphics2.cpp
+++ Lab2/LabGraphics2.cpp
@@ -1,44 +1,55 @@
 ﻿#include <iostream>
 #include <Windows.h>
 #include <cmath>
+#include <chrono>
+#include <thread>
 
 HWND hwnd = GetConsoleWindow();
 HDC hdc = GetDC(hwnd);
 RECT window = {};
 const double PI = 3.1415926535897932384;
 
-//tjj
-void drawPifagorsTree(int x, int y, int l, double radian)
+void drawPifagorsTree(int x, int y, int lenght, double radian, int depth)
 {
-    if (l <= 20)
+    if (lenght <= 20)
         return;
 
-    MoveToEx(hdc, x, y, NULL);
-    LineTo(hdc, x + l * cos(radian + PI / 6), y - l * sin(radian + PI / 6));
+    HPEN myPen = CreatePen(PS_SOLID, 9 - depth, RGB(0, 14 * depth + 40, 0));
+    SelectObject(hdc, myPen);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(35));
 
     MoveToEx(hdc, x, y, NULL);
-    LineTo(hdc, x + l * cos(radian - PI / 6), y - l * sin(radian - PI / 6));
+    LineTo(hdc, x + lenght * cos(radian + PI / 6), y - lenght * sin(radian + PI / 6));
 
-    drawPifagorsTree(x + l * cos(radian + PI / 6), y - l * sin(radian + PI / 6), l / 1.3, radian + PI / 6);
-    drawPifagorsTree(x + l * cos(radian - PI / 6), y - l * sin(radian - PI / 6), l / 1.3, radian - PI / 6);
+    drawPifagorsTree(x + lenght * cos(radian + PI / 6), y - lenght * sin(radian + PI / 6), lenght / 1.3, radian + PI / 6, depth + 1);
+
+    SelectObject(hdc, myPen);
+
+    MoveToEx(hdc, x, y, NULL);
+    LineTo(hdc, x + lenght * cos(radian - PI / 6), y - lenght * sin(radian - PI / 6));
+
+    drawPifagorsTree(x + lenght * cos(radian - PI / 6), y - lenght * sin(radian - PI / 6), lenght / 1.3, radian - PI / 6, depth + 1);
+
+    DeleteObject(myPen);
 }
 
 int main()
 {
-    SelectObject(hdc, GetStockObject(WHITE_PEN));
-    SelectObject(hdc, GetStockObject(WHITE_BRUSH));
+    HPEN myPen = CreatePen(PS_SOLID, 9, RGB(0, 40, 0));
+    SelectObject(hdc, myPen);
 
-    int x = 1000, y = 900, l = 200;
+    int x = 1000, y = 900, lenght = 200;
     double radian = PI / 2;
     std::getchar();
 
     MoveToEx(hdc, x, y, NULL);
-    y -= l * sin(radian);
+    y -= lenght * sin(radian);
     LineTo(hdc, x, y);
 
-    l /= 1.3;
+    lenght /= 1.3;
 
-    drawPifagorsTree(x, y, l, radian);
+    drawPifagorsTree(x, y, lenght, radian, 1);
 
     ReleaseDC(hwnd, hdc);
 
