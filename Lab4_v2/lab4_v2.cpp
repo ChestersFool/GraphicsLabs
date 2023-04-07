3cb84b48f10cf98de7e2d01ac6de4ba6f84f88a4
Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-04-07 05:11:19 +0300

    1

--- Lab4_v2/lab4_v2.cpp
+++ Lab4_v2/lab4_v2.cpp
@@ -6,7 +6,7 @@ const double PI = 3.1415926535897932384;
 HINSTANCE hInst;
 HWND hwndMain;
 
-CPoint camera(0, 0, 400);
+CPoint camera(0, 0, 200);
 C3DRectangle first(CPoint(-50, -50, 0), CPoint(-50, -100, 0), CPoint(-100, -100, 0), CPoint(-100, -50, 0), CPoint(-50, -50, 50), CPoint(-50, -100, 50), CPoint(-100, -100, 50), CPoint(-100, -50, 50));
 C3DRectangle second(CPoint(-50, 50, 0), CPoint(-50, 100, 0), CPoint(-100, 100, 0), CPoint(-100, 50, 0), CPoint(-50, 50, 50), CPoint(-50, 100, 50), CPoint(-100, 100, 50), CPoint(-100, 50, 50));
 RotationMatrix m(PI / 100);
@@ -21,19 +21,17 @@ LRESULT APIENTRY WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_PAINT: // ! DRAWING
         hdc = GetDC(hwndMain);
 
-        SelectObject(hdc, GetStockObject(BLACK_PEN));
-
         Rectangle(hdc, 0, 0, WINDOW_SIZE, WINDOW_SIZE);
 
         drawAxis(hdc, WINDOW_SIZE);
 
-        rorateX3DRectangle(first, PI / 120);
+        rorateX3DRectangle(first, PI / 100);
         draw3DRectangle(first, camera, hdc, WINDOW_SIZE);
 
-        rorateY3DRectangle(second, PI / 120);
+        rorateY3DRectangle(second, PI / 100);
         draw3DRectangle(second, camera, hdc, WINDOW_SIZE, true);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(17));
+        std::this_thread::sleep_for(std::chrono::milliseconds(34));
 
         ReleaseDC(hwndMain, hdc);
         return 0;
---
+++ Lab4_v2/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+    case WM_PAINT: // ! DRAWING
